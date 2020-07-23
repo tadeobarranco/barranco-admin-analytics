f0e417375a5b8a852d1f67d9b7b742a2c9375392
Tadeo Barranco <tbarrancoa@gmail.com>
2020-07-23 08:12:07 -0500

    Add required files to build a Magento MODULE

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * register method: 1' 03"
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Barranco_AdminAnalytics',
+    __DIR__
+);
